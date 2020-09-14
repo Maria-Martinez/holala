96481d5d86cad878dab1da3786b68df674c75c1a
maria martinez <mariaj.martinez.2019@upb.edu.co>
2020-09-14 17:42:25 -0500

    hi

---
+++ src/holali/Hello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holali;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author maria_000
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ //       System.out.println("hello world :3");                             //forma normal de imprimir el mensaje
+          JOptionPane.showMessageDialog(null, "Hello World! :3 ");          //forma grafica de imprimir el mensaje
+    }
+    
+}
